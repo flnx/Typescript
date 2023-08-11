9a8db47dd2b69291a4324dc653f39e3c07def0f9
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-08-11 06:55:54 +0300

    Tyipescript: variables and any notes

---
+++ 01. Intro/any.tsx
@@ -0,0 +1,12 @@
+// NOTE: Not a good practice
+
+// "Any" is a market to tell Typescript that we don't want to do type checking
+
+// let hero; // if a type is not assigned, it will automatically be set to "any"
+let hero: string; // don't 
+
+function getHero() {
+    return "thor"
+}
+
+hero = getHero();
---
+++ 01. Intro/variables.tsx
@@ -0,0 +1,11 @@
+// NOTE: Not always a good practice to specify the variables types like that
+
+let greetings: string = "Hello, FLN";
+
+let userId: number = 123;
+
+let isLoggedIn: boolean = false;
+
+export { };
+
+// tsc variables.tsx
