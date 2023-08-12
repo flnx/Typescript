9cc7fde6a88ea30aabfffc7306d645402f682ff5
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-08-12 13:24:36 +0300

    type aliases

---
+++ 04. Type Aliases/1.tsx
@@ -0,0 +1,19 @@
+type User = {
+    readonly _id: string;
+    name: string;
+    email: string;
+    isActive: boolean;
+    creditCardDetails?: number // optional
+};
+
+const myUser: User = {
+    _id: '12345',
+    name: 'Thor',
+    email: 'thor@yahoo.com',
+    isActive: false,
+};
+
+myUser.email = "thor@gmail.com"
+// myUser._id = "123" // not allowed
+
+
